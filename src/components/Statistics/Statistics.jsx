1532686c2ad9bd13c121dc965e9f773351100991
AnttonnIT <anttonnit@gmail.com>
2023-02-15 14:47:28 +0200

    up

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,51 +1,40 @@
-import css from './Statistics.module.css';
 import PropTypes from 'prop-types';
+import { List, ListItem } from './Statistics.styled';
 
-const createColor = () => {
-  const color =
-    'rgba(' +
-    Math.round(Math.random() * 255) +
-    ',' +
-    Math.round(Math.random() * 255) +
-    ',' +
-    Math.round(Math.random() * 255) +
-    ',' +
-    0.5 +
-    ')';
+export const Statistics = ({
+  total,
+  good,
+  neutral,
+  bad,
+  positivePercentage,
+}) => {
+  const options = {
+    good,
+    neutral,
+    bad,
+    total,
+    positiveFeedback: positivePercentage,
+  };
 
-  return color;
-};
-
-export const Statistics = ({ title, stats }) => {
   return (
-    <section className={css.statistics}>
-      {title && <h2 className={css.title}>Upload stats</h2>}
-
-      <ul className={css.statList}>
-        {stats.map(({ id, label, percentage }) => {
+    <>
+      <List>
+        {Object.entries(options).map(([key, value]) => {
           return (
-            <li
-              key={id}
-              className={css.item}
-              style={{ backgroundColor: createColor() }}
-            >
-              <span className={css.label}>{label}</span>
-              <span className={css.percentage}>{percentage}%</span>
-            </li>
+            <ListItem key={key}>
+              {key}: <span>{value}</span>
+            </ListItem>
           );
         })}
-      </ul>
-    </section>
+      </List>
+    </>
   );
 };
 
 Statistics.propTypes = {
-  title: PropTypes.string,
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ).isRequired,
+  total: PropTypes.number.isRequired,
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
 };
